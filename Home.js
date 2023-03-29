dce665027e042e73b7faa5d6ed8c20f2660d2c65
Allavinodkumar04 <129280127+Allavinodkumar04@users.noreply.github.com>
2023-03-29 16:59:26 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <centet>
+
+    <h4>Home component</h4>            
+            </centet>
+        </div>
+    )
+}
+
+export default Home
---
+++ Orders.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Orders = () => {
+    return (
+        <div>
+            <centet>
+
+    <h4>Orders component</h4>            
+            </centet>
+        </div>
+    )
+}
+
+export default Orders
